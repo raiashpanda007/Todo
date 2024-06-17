e993589a956e8bbd7a8b8a67e1c259e8b468ff7c
Ashwin Rai <ashwin.2201098cs@iiitbh.ac.in>
2024-06-17 08:00:04 +0530

    chore: Update base URL for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/todoproject/', // Base URL for GitHub Pages deployment
+  base: '/Todo/', // Base URL for GitHub Pages deployment
   plugins: [react()],
 });
